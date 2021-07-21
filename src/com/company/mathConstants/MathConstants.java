0711d59126615fd4a0406edb8f5afe372523238e
Linda-Ju <linda.junkina@gmail.com>
2021-07-21 18:42:03 +0300

    Enums UML

--- src/com/company/mathConstants/MathConstants.java
+++ src/com/company/mathConstants/MathConstants.java
@@ -1,19 +1,19 @@
 package com.company.mathConstants;
 
 public enum MathConstants {
-    PI("3.14"),
-    E("2.71828"),
-    THE_GOLDEN_RATIO("1.618033"),
-    GRAVITATION_CONST("9.8m/s^"),
-    THE_SPEED_OF_LIGHT("299792458 m/s");
+    PI(3.14),
+    E(2.71828),
+    THE_GOLDEN_RATIO(1.618033),
+    GRAVITATION_CONST(9.8),
+    THE_SPEED_OF_LIGHT(299792458);
 
-    private final String number;
+    private final double number;
 
-    MathConstants(String number) {
+    MathConstants(double number) {
         this.number = number;
     }
 
-    public String getNumber() {
+    public double getNumber() {
         return number;
     }
 }
